9ae0676deabf207a157da2e47c9bb13c339c8179
Zach Tackett <ztackett11@gmail.com>
2022-01-04 12:03:29 -0500

    Add login POST route

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData = [
     {name: 'Mark'},
     {name: 'Jill'}
@@ -22,6 +25,29 @@ app.get('/users/:id', function(req, res) {
         message: 'successfully got one user.',
         users: req.params.id
     });
+});
+
+app.post('/login', function(req, res) {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = 'ztackett11'
+    const mockPassword = 'password123';
+
+    if (username == mockUsername && password == mockPassword) {
+        res.json({
+            success: true,
+            message: 'login successful',
+            token: 'encrypted token goes here'
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'Username or password does not match. try again...'
+        });
+    }
+    
 });
 
 app.listen(8000, function() {
